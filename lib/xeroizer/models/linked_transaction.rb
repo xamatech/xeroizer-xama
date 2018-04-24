7c1022653e4d7913623e7e20dd2c9c70446cb6f9
Christian Baus <cbaus@Christians-MacBook-Pro-2.local>
2018-04-24 18:09:16 +0200

    changing linked_transactions

--- lib/xeroizer/models/linked_transaction.rb
+++ lib/xeroizer/models/linked_transaction.rb
@@ -2,21 +2,21 @@ module Xeroizer
   module Record
 
     class LinkedTransactionModel < BaseModel
-
       set_permissions :read, :write, :update
-
     end
 
     class LinkedTransaction < Base
       set_primary_key :linked_transaction_id
 
       guid :linked_transaction_id
-      string :source_transaction_i_d
-      string :source_line_item_i_d
+      string :source_transaction_id, :api_name => 'SourceTransactionID'
+      string :source_line_item_id, :api_name => 'SourceLineItemID'
       string :page
-      string :contact_i_d
-      string :target_transaction_id
+      string :contact_id, :api_name => 'ContactID'
+      string :target_transaction_id, :api_name => 'TargetTransactionID'
+      string :target_line_item_id, :api_name => 'TargetLineItemID'
       string :status
+      datetime_utc :updated_date_utc, :api_name => 'UpdatedDateUTC'
     end
 
   end
